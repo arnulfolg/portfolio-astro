8e878c5187a1bcae1f5b2614b08c4fe765413e49
Arnulfo Loredo <arnulfo.lg@live.com>
2024-08-13 00:28:18 -0400

    Added Contentful configuration and locale function

---
+++ src/scripts/getLocale.ts
@@ -0,0 +1,14 @@
+const Languages = {
+	ES: 'es-419',
+  	EN: 'en-US',
+}
+
+export const getLocale = (locale: string) => {
+	const language = locale.trim().toUpperCase()
+	
+	if(Object.keys(Languages).includes(language)) {
+		return Languages[language]
+	} else {
+		return Languages.EN
+	}
+}
---
+++ test/getLocale.test.ts
@@ -0,0 +1,23 @@
+import { expect, test } from 'vitest';
+import { getLocale } from '../src/scripts/getLocale';
+ 
+
+test('getLocale es should return spanish code', () => {
+	const result = getLocale('es');
+	expect(result).toBe('es-419');
+});
+
+test('getLocale en should return english code', () => {
+	const result = getLocale('en');
+	expect(result).toBe('en-US');
+});
+
+test('getLocale empty should return english code', () => {
+	const result = getLocale('');
+	expect(result).toBe('en-US');
+});
+
+test('getLocale fr should return english code', () => {
+	const result = getLocale('fr');
+	expect(result).toBe('en-US');
+});
