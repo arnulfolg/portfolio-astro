08cabb3599b19d7b8bf9d0cc42a2c9cd3901b71f
Arnulfo Loredo <arnulfo.lg@live.com>
2024-08-13 00:46:36 -0400

    Added button to change site language

--- src/scripts/getLocale.ts
+++ src/scripts/getLocale.ts
@@ -7,6 +7,7 @@ export const getLocale = (locale: string) => {
 	const language = locale.trim().toUpperCase()
 	
 	if(Object.keys(Languages).includes(language)) {
+		//@ts-ignore
 		return Languages[language]
 	} else {
 		return Languages.EN
