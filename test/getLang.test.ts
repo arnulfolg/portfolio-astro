be69db962ebaf74b7ca4e6ba8edced3e0b5dacd6
Arnulfo Loredo <arnulfo.lg@live.com>
2024-08-14 23:39:33 -0400

    Added tests

---
+++ test/getLang.test.ts
@@ -0,0 +1,15 @@
+import { expect, test } from 'vitest';
+import { getLang } from '../src/scripts/getLang';
+
+test('getLang should return en code', () => {
+	const pathname = '/en/'
+	const result = getLang(pathname);
+	expect(result).toBe('en');
+});
+
+test('getLang should return es code', () => {
+	const pathname = '/es/'
+	const result = getLang(pathname);
+	expect(result).toBe('es');
+});
+
---
+++ test/getLocaleAndLang.test.ts
@@ -0,0 +1,17 @@
+import { expect, test } from 'vitest';
+import { getLocaleAndLang } from '../src/scripts/getLocaleAndLang';
+
+test('getLocaleAndLang should return en code', () => {
+	const pathname = '/en/'
+	const [language, locale] = getLocaleAndLang(pathname);
+	expect(language).toBe('en');
+	expect(locale).toBe('en-US');
+});
+
+test('getLocaleAndLang should return es code', () => {
+	const pathname = '/es/'
+	const [language, locale] = getLocaleAndLang(pathname);
+	expect(language).toBe('es');
+	expect(locale).toBe('es-419');
+});
+
